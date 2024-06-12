4262828f5f3e84c250ee5d15ab8e22d4d61ce878
Jamil Muhammad, ABDULLAHI <130340582+bnusmann22@users.noreply.github.com>
2024-06-12 07:03:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,55 @@
+var cartQuantity = 0;
+const result = document.getElementById("result");
+const price = document.getElementById("price");
+
+function showQuantity(){
+  if (cartQuantity == 0) {
+    alert("Cart is empty: Please Select an item");
+  }
+    else if(cartQuantity == 1){
+      result.textContent =(`${cartQuantity} item was selected`);
+    }
+    else{
+      result.textContent =(`${cartQuantity} items where selected`);
+  } 
+}
+function decrement(){
+  if (cartQuantity == 0) {
+    alert("Cart is empty: can't remove an item");
+  }
+    else{
+    cartQuantity -- 
+    alert("An Item was removed from cart")
+    result.textContent = ("Cart Quantity:" + " " + cartQuantity);
+  }
+}
+
+
+function reset(){
+  cartQuantity = 0;
+  alert("Cart Quantity was reset");
+  result.textContent = ("Cart Quantity:" + " " + cartQuantity);
+}
+
+function increment1(){
+  cartQuantity ++
+  result.textContent = ("Cart Quantity:" + " " + cartQuantity);
+}
+
+
+function increment2(){
+  cartQuantity += 2
+  result.textContent = ("Cart Quantity:" + " " + cartQuantity);
+}
+function increment3(){
+  cartQuantity += 3
+  result.textContent = ("Cart Quantity:" + " " + cartQuantity);
+}
+
+function getPrice() {
+  let amount = 25;
+  getPrice = cartQuantity * amount;
+
+  price.textContent = "$" + "" + getPrice;
+  return;
+}
